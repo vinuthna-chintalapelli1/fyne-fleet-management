9820ddd392560bd30c2f437a86a134603dcc67c0
vinuthna singireddy <vinuthna.chintalapelli@hpe.com>
2025-03-19 14:59:50 -0400

    add toolbox

--- main.go
+++ main.go
@@ -1,13 +1,66 @@
 package main
 
 import (
+	"log"
+	"net/url"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+// Function to open the toolbox of URLs
+func openToolbox(a fyne.App) {
+	// Create URL objects for different services
+	confluenceURL, err := url.Parse("https://confluence.storage.hpecorp.net/display/NCS/DSCC")
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	grafanaURL, err := url.Parse("https://fleetpoc2-us-west-2.cloudops.qa.cds.hpe.com/grafana/d/uid_search_data_processor/search-data-processor?orgId=1")
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	humioURL, err := url.Parse("https://fleetpoc2-us-west-2.cloudops.qa.cds.hpe.com/logs/storagecentral/search")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	pavoURL, err := url.Parse("https://console-neonops3-app.qa.cds.hpe.com/")
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Create the content for the toolbox
+	hello := widget.NewLabel("Welcome to the toolbox!")
+
+	toolboxWindow := a.NewWindow("Toolbox of Handy URLs")
+	toolboxWindow.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome to the handy links!")
+		}),
+		widget.NewHyperlink("Confluence", confluenceURL),
+		widget.NewHyperlink("Grafana", grafanaURL),
+		widget.NewHyperlink("Humio", humioURL),
+		widget.NewHyperlink("Pavo / Aquilla", pavoURL),
+	))
+
+	toolboxWindow.Resize(fyne.NewSize(400, 300)) // Resize for better appearance
+	toolboxWindow.Show()
+}
+
 func main() {
+	// Initialize Fyne app and main window
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Fleet Management")
 
@@ -61,8 +114,13 @@ func main() {
 		selectedLabel,
 	)
 
-	// Add components to the window
-	myWindow.SetContent(container.NewVBox(topPanel, table))
+	// Button to open the toolbox of URLs
+	openToolboxButton := widget.NewButton("Open Toolbox", func() {
+		openToolbox(myApp)
+	})
+
+	// Add components to the main window
+	myWindow.SetContent(container.NewVBox(topPanel, table, openToolboxButton))
 	myWindow.Resize(fyne.NewSize(800, 600)) // Resize the window to make sure it's big enough for the table
 
 	// Show the window
